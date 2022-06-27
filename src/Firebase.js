1c8e447b0b49c6ef33a57efe3a9462a0ce97c7e6
Onur Kanmaz <onurkanmaz@gmail.com>
2022-06-27 14:47:31 -0500

    Init

---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA7h8BZJUTp1J1lEy-AFJCGFIrnkomD55k",
+    authDomain: "item-manager-707c2.firebaseapp.com",
+    projectId: "item-manager-707c2",
+    storageBucket: "item-manager-707c2.appspot.com",
+    messagingSenderId: "858332584629",
+    appId: "1:858332584629:web:5ae15d447d5510384b7cfc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
